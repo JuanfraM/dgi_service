26c0c80610fe78b6453bfcc9c5fb25b8d7fd736b
JuanfraM <juanframagrini@gmail.com>
2017-04-26 00:00:30 -0300

    added some rescue errors

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -3,12 +3,23 @@ module Api
     respond_to :json
     layout false
 
-    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-    rescue_from ActiveRecord::RecordInvalid,  with: :record_not_found
+    rescue_from ActiveRecord::RecordNotFound,       with: :record_not_found
+    rescue_from ActiveRecord::RecordInvalid,        with: :record_not_found
+    rescue_from Exception::NoMethodError,           with: :method_not_found
+    rescue_from ActionController::RoutingError,     with: :route_not_found
+    rescue_from AbstractController::ActionNotFound, with: :method_not_found
 
     # Returns the API status
     def status
       render json: { online: true }
+    end
+
+    def method_not_found
+      render json: { error: 'Method or Action not found' }, status: 403
+    end
+
+    def route_not_found
+      render json: { error: 'Route not found' }, status: 404
     end
 
     def record_not_found
